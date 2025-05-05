3e36c01ba22bf09312ba5a9c2783b505d9abb55f
Eduardo Ureta <eduardo.uretaca@gmail.com>
2025-05-05 14:35:16 -0400

    favicon

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(express.urlencoded({ extended: true}));
 
 // CORS
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: ['http://localhost:5173', 'https://eduardoureta.xyz/', 'http://eduardoureta.xyz/'],
     credentials: true, 
 }));
 
@@ -22,9 +22,9 @@ app.use('/api/animales', AnimalesRoutes);
 app.use('/api/usuarios', UsuariosRoutes);
 app.use('/api/auth', SessionsRoutes);
 
-// app.use("/*", (req, res) => {
-//     res.sendFile(path.join(process.cwd(),"public/index.html"));
-// });
+app.use("/", (req, res) => {
+    res.sendFile(path.join(process.cwd(),"public/index.html"));
+});
 
 app.listen(port, () => {
     console.log(`Servidor en puerto ${port}`);
