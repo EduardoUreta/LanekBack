7917c20998e0e6c803928996c4240d715f10d6f1
Eduardo Ureta <eduardo.uretaca@gmail.com>
2025-05-05 15:32:27 -0400

    .

--- config/config.cjs
+++ config/config.cjs
@@ -19,8 +19,15 @@ const config = process.env.DATABASE_URL
       host: process.env.DB_HOST,
       port: process.env.DB_PORT,
       dialect: "postgres",
+      dialectOptions: {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false,
+        },
+      },
       JWT_SECRET: process.env.JWT_SECRET
     };
+
 
 module.exports = config;
 
