ecb9633e19d75b3a5e3962d0b5f7b6ef6e7b9a38
Eduardo Ureta <eduardo.uretaca@gmail.com>
2025-05-16 12:54:03 -0400

    Sequelize

--- config/config.cjs
+++ config/config.cjs
@@ -1,35 +1,29 @@
 require('dotenv').config();
 
-const config = process.env.DATABASE_URL
-  ? {
-      username: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_DATABASE,
-      host: process.env.DB_HOST,
-      port: process.env.DB_PORT,
-      dialect: "postgres",
-      dialectOptions: {
-        ssl: {
-          require: true,
-          rejectUnauthorized: false,
+module.exports = {
+  development: process.env.DATABASE_URL
+    ? {
+        url: process.env.DATABASE_URL,
+        dialect: "postgres",
+        dialectOptions: {
+          ssl: {
+            require: true,
+            rejectUnauthorized: false,
+          },
+        },
+      }
+    : {
+        username: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_DATABASE,
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        dialect: "postgres",
+        dialectOptions: {
+          ssl: {
+            require: true,
+            rejectUnauthorized: false,
+          },
         },
       },
-      JWT_SECRET: process.env.JWT_SECRET
-    }
-  : {
-      url: process.env.DATABASE_URL, 
-      dialect: "postgres",
-      dialectOptions: {
-        ssl: {
-          require: true,
-          rejectUnauthorized: false,
-        },
-      },
-      JWT_SECRET: process.env.JWT_SECRET
-    };
-
-
-module.exports = config;
-
-
-
+};
