f31ed98b59d91658261c1fb86484f4d2b722ccd8
Eduardo Ureta <eduardo.uretaca@gmail.com>
2025-05-19 17:22:56 -0400

    CAMBIOS EN CONFIG.CJS

--- config/config.cjs
+++ config/config.cjs
@@ -2,6 +2,13 @@
 
 const config = {
     url: process.env.DATABASE_URL, 
+    dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
     JWT_SECRET: process.env.JWT_SECRET
   }; 
 
