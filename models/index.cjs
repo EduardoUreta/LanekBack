4a45c2511c478e42b1ddf0f0f39bd332c4c0fb6b
Eduardo Ureta <eduardo.uretaca@gmail.com>
2025-05-19 17:08:04 -0400

    CAMBIOS EN CONFIG.CJS

--- models/index.cjs
+++ models/index.cjs
@@ -11,7 +11,7 @@ const db = {};
 let sequelize;
 
 if (config.url) {
-  sequelize = new Sequelize(config.url);
+  sequelize = new Sequelize('postgresql://postgres:eduardo-test-db@eduardo-test-db.ccffs1c8f6p9.us-east-2.rds.amazonaws.com:5432/eduardo-test-db');
   console.log("Conectando a:", config.host, config.database);
 }
 
